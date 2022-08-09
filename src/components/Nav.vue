c56d04262998cbd28d721c3c52ad093a06ed0ff9
Tacitus <kakmsaikou@163.com>
2022-08-09 17:00:10 +0800

    添加 svgo-loader

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/statistic" class="item">
+    <router-link to="/statistic" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -26,7 +26,6 @@
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
     font-size: 12px;
-    color: #999;
     > .item {
       padding: 2px 0;
       flex-grow: 1;
@@ -34,10 +33,14 @@
       justify-content: center;
       align-items: center;
       flex-direction: column;
+      color: #999;
       .icon{
         width: 32px;
         height: 32px;
       }
+    }
+    > .item.selected{
+      color: #59F;
     }
   }
 </style>
