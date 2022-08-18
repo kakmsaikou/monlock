74882c98397d9a4ee79414a20604884e820a2f75
TacitusPie <kakmsaikou@gmail.com>
2022-08-18 10:09:35 +0800

    删除 Types.vue

--- src/views/Statistic.vue
+++ src/views/Statistic.vue
@@ -9,14 +9,13 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import Types from '@/components/Money/Types.vue';
   import Tabs from '@/components/Tabs.vue';
   import {Component} from 'vue-property-decorator';
   import intervalList from '@/constants/intervalList';
   import recordTypeList from '@/constants/recordTypeList';
 
   @Component({
-    components: {Types, Tabs},
+    components: {Tabs},
   })
   export default class statistic extends Vue {
     type = '-';
