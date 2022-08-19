fdbc149a1130981e4ecc4cffaa069d2ad666e382
TacitusPie <kakmsaikou@gmail.com>
2022-08-19 15:04:52 +0800

    保证用户用手机打开页面，否则就提示用户

--- src/main.ts
+++ src/main.ts
@@ -17,3 +17,17 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app');
+
+console.log();
+if (matchMedia('(pointer:fine)').matches) {
+  window.alert('请使用手机打开本页面，以保证浏览效果。');
+  const img = document.createElement('img')
+  img.src = '/img/qrcode.png'
+  img.style.position = 'fixed'
+  img.style.left = '50%'
+  img.style.top = '50%'
+  img.style.transform = 'translate(-50%,-50%)'
+  img.style.height = '50vh'
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,0.05)'
+  document.body.appendChild(img)
+}
