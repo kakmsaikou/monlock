c64bd8ca3cd5b3495fb98b8ba49058d52d87c142
TacitusPie <kakmsaikou@gmail.com>
2022-10-07 16:53:01 +0800

    deploy

---
+++ precache-manifest.cd706dc1558fb9107ba2acdccade383e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d8688733e3eb5b09166",
+    "url": "/sherlock/css/app.2618bfb8.css"
+  },
+  {
+    "revision": "5e712377d9d1c6ce417114fdda87bff8",
+    "url": "/sherlock/img/qrcode.5e712377.png"
+  },
+  {
+    "revision": "f2e9f175698ec856a1827f03fa468544",
+    "url": "/sherlock/index.html"
+  },
+  {
+    "revision": "2d8688733e3eb5b09166",
+    "url": "/sherlock/js/app.79e90474.js"
+  },
+  {
+    "revision": "c0b21944a62ef26fbbc3",
+    "url": "/sherlock/js/chunk-vendors.1b88efea.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/sherlock/logo.png"
+  },
+  {
+    "revision": "9d1c4fc3bd1a1fd6b6a05948b39ca259",
+    "url": "/sherlock/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sherlock/robots.txt"
+  }
+]);
